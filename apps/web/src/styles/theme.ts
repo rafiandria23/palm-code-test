5753ac7d4317ad73e3b5cabd69c1847c0267cc7e
Adam Rafiandri <rafiandria23@gmail.com>
2024-05-10 12:26:45 +0700

    feat(web)!: initial development of page background

---
+++ apps/web/src/styles/theme.ts
@@ -0,0 +1,44 @@
+'use client';
+
+import {
+  Bodoni_Moda as BodoniModaFont,
+  Inter as InterFont,
+} from 'next/font/google';
+import { createTheme } from '@mui/material';
+
+const bodoniModaFont = BodoniModaFont({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '600', '700', '800', '900'],
+});
+const interFont = InterFont({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: interFont.style.fontFamily,
+    h1: {
+      fontFamily: bodoniModaFont.style.fontFamily,
+    },
+    h2: {
+      fontFamily: bodoniModaFont.style.fontFamily,
+    },
+    h3: {
+      fontFamily: bodoniModaFont.style.fontFamily,
+    },
+    h4: {
+      fontFamily: bodoniModaFont.style.fontFamily,
+    },
+    h5: {
+      fontFamily: bodoniModaFont.style.fontFamily,
+    },
+    h6: {
+      fontFamily: bodoniModaFont.style.fontFamily,
+    },
+  },
+});
+
+export default theme;
